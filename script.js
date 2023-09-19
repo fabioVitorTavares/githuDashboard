542118ee9cebf88ac7dd96379a7efde7af7b5469
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-09-18 22:00:34 -0300

    commit

--- script.js
+++ script.js
@@ -11,5 +11,30 @@ async function fetchData() {
 
   console.log("Log line 15: ", data);
 }
-
 fetchData();
+// }
+// async function fetchData() {
+//   fetch("https://api.github.com/repos/fabioVitorTavares/githubDashboard").then(
+//     (value) => {
+//       console.log("Log line 17: ", value);
+//       value.json().then((json) => {
+//         console.log("Log line 19: ", json);
+//       });
+//     }
+//   );
+// }
+
+// async function fetchRepo(repo) {
+//   const response = await fetch(
+//     `https://api.github.com/repos/fabioVitorTavares/${repo}`
+//   );
+//   const data = await response.json();
+//   console.log("Log line 29: ", { data });
+
+//   const resposeCommits = await fetch(data?.commits_url);
+//   const dataCommits = await resposeCommits.json();
+
+//   console.log("Log line 35: ", { dataCommits });
+// }
+
+// fetchRepo("githubDashboard");
